3a4ed11b7cb1bea5d8d7a7a97e2fef93bf3e44e5
Andrew Irwin <someonefromcanada38@gmail.com>
2016-06-03 19:36:52 +0000

    uploaded to git

---
+++ app/urlShortener.js
@@ -0,0 +1,74 @@
+module.exports = function(app, db) {
+var primaryURL = process.env.APP_URL;
+
+  app.route('/:url')
+  .get(shortLink);
+  
+  app.get('/new/:url*', newLink)
+  
+  function newLink(req, res)
+  {
+    console.log('Requested Shortened Url: '+req.url.slice(5));
+    var json = {};
+    if(testUrl(req.url.slice(5)))
+    {
+        json = {
+          "originalURL": req.url.slice(5),
+          "shortURL": primaryURL + shortURL()
+        };
+        saveLink(json, db);
+    }
+    else
+    {
+        json = {
+          "error": "Wrong URL Format, Please Try Again."  
+        };
+    }
+    res.send(json);
+  }
+  
+  function shortLink(req,res)
+  {
+    console.log('Requested Url: ' + req.url.slice(1));
+    var link = primaryURL + req.url.slice(1);
+    console.log(link);
+    var sites = db.collection('sites');
+    
+    sites.findOne({
+      "shortURL": link
+    }, function(err, result){
+      if(err) throw err;
+      if(result){
+        console.log()
+      res.redirect(result.originalURL);  
+      }
+      else
+      {
+        res.send({"error": "This URL is not in the database"});
+      }
+    });
+  }
+  function saveLink(link, db)
+  {
+    console.log('Saving Link: '+ link.originalURL);
+    var sites = db.collection('sites');
+    sites.save(link, function(err, result){
+       if (err) throw err;
+       console.log('Saved');
+    });
+  }
+  
+  function shortURL()
+  {
+    var num = Math.floor(1000 + Math.random() * 899999);
+    return num.toString().substring(0,4);
+  }
+  
+  function testUrl(url)
+  {
+    //a regular expression for testing a urls validity
+    var urlexpression = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i
+    return urlexpression.test(url);
+  }
+
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var mongo = require('mongodb');
+var urlapi = require('./app/urlShortener.js');
+var dburl = 'mongodb://admin:password@ds015403.mlab.com:15403/urls'
+
+var app = express();
+
+mongo.MongoClient.connect(dburl, function(err, db) {
+
+  if (err) {
+    throw new Error('Error: Database not connected');
+  } else {
+    console.log('Connected to MongoDB');
+  }
+  
+  db.createCollection("sites", {
+    capped: true,
+    size: 500000,
+    max: 500
+  });
+  
+  urlapi(app, db);
+  
+  var port = 8080;
+  app.listen(port, function() {
+    console.log('Node.js listening on port ' + port);
+  });
+  
+});
