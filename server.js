6c75395d087ad01daa9913b4abbdb8bed932c71b
Andrew Irwin <someonefromcanada38@gmail.com>
2016-06-06 18:15:31 +0000

    updated

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 var express = require('express');
-var mongo = require('mongodb');
-var urlapi = require('./app/urlShortener.js');
-var dburl = 'mongodb://admin:password@ds015403.mlab.com:15403/urls'
+var mongoose = require('mongoose');
+var searchapi = require('./app/searchapi.js');
+var dbsearches = process.env.MONGO_URI;
+require('dotenv').config({
+  silent: true
+});
 
 var app = express();
 
-mongo.MongoClient.connect(dburl, function(err, db) {
+var db = mongoose.connection;
+  var historySchema = new mongoose.Schema({
+  search: String,
+  date: String
+});
+var history = mongoose.model('history', historySchema);
 
-  if (err) {
-    throw new Error('Error: Database not connected');
-  } else {
-    console.log('Connected to MongoDB');
-  }
-  
-  db.createCollection("sites", {
-    capped: true,
-    size: 500000,
-    max: 500
-  });
-  
-  urlapi(app, db);
+mongoose.connect(dbsearches);
+db.on('error', console.error.bind(console, 'connection error:'));
+con
+db.once('open', function() {
+  // we're connected!
+});
+
+  searchapi(app, history);
   
   var port = process.env.PORT || 8080;
   app.listen(port, function() {
     console.log('Node.js listening on port ' + port);
   });
-  
-});
