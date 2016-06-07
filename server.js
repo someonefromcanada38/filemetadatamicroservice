a65c42c9198822ac8aa4b582c221126f1c75bda3
Andrew Irwin <someonefromcanada38@gmail.com>
2016-06-07 16:22:16 +0000

    uploaded

--- server.js
+++ server.js
@@ -1,30 +1,18 @@
 var express = require('express');
-var mongoose = require('mongoose');
-var searchapi = require('./app/searchapi.js');
-var dbsearches = process.env.MONGO_URI;
-require('dotenv').config({
-  silent: true
-});
+var multer = require('multer');
+var upload = multer({ dest: "./uploads" });
 
 var app = express();
+app.use(express.static(__dirname + '/public'));
 
-var db = mongoose.connection;
-  var historySchema = new mongoose.Schema({
-  search: String,
-  date: String
-});
-var history = mongoose.model('history', historySchema);
-
-mongoose.connect(dbsearches);
-db.on('error', console.error.bind(console, 'connection error:'));
-console.log("db connected");
-db.once('open', function() {
-  // we're connected!
+app.get('/', function(req, res) {
+    console.log('html file served');
+    res.sendfile("index.html");
 });
 
-  searchapi(app, history);
-  
-  var port = process.env.PORT || 8080;
-  app.listen(port, function() {
-    console.log('Node.js listening on port ' + port);
-  });
+app.post('/file', upload.single('check'), function(req,res){
+  var filesize = {filesize: req.file.size + " bytes"}
+  res.send(filesize);
+});
+
+app.listen(process.env.PORT || 8080);
